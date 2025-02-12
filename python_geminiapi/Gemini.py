67e96b842f60403bcb1e8dcf6b1bf0341706d24e
Bharathreddy459 <69196438+Bharathreddy459@users.noreply.github.com>
2025-02-12 23:44:58 +0530

    Add files via upload

---
+++ python_geminiapi/Gemini.py
@@ -0,0 +1,28 @@
+import os
+import google.generativeai as genai
+
+genai.configure(api_key = 'AIzaSyBQh8KpyjRsEDGAuaud7fEmQZhiicKwyOE')
+
+# Create the model
+# generation_config = {
+#   "temperature": 1,
+#   "top_p": 0.95,
+#   "top_k": 40,
+#   "max_output_tokens": 8192,
+#   "response_mime_type": "text/plain",
+# } 
+
+model = genai.GenerativeModel(
+  model_name="gemini-2.0-flash",
+  #generation_config=generation_config,
+)
+
+# chat_session = model.start_chat(
+#   history=[
+#   ]
+# )
+#response = chat_session.send_message("5 word sentence")
+
+
+response = model.generate_content("Is python better than javascript in 50 words")
+print(response.text)
